12309cfbdeffb4a98842cdd015ef7a84da0843b7
josegabrielfc <jgfchona@gmail.com>
2023-12-05 22:48:36 -0500

    Backend&Frontend

---
+++ Backend/src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan"); //Se utiliza para el registro (logging) de solicitudes HTTP
+const cors = require('cors');
+
+const app = express();
+const api = require("./routes/index");
+
+//Settings
+app.set("port", process.env.PORT || 3001);
+app.set("json spaces", 2);
+
+//Middleware
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.use("/", api);
+app.use("/login", (req, res) => {
+  res.render("");
+});
+
+module.exports = app;
