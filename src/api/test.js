ab7188a244d3c80da8384186b3c643bd08fd8a94
jdprietom03 <jdprietom@eafit.edu.co>
2024-04-24 01:06:07 -0500

    Test deployment

--- src/api/test.js
+++ src/api/test.js
@@ -1,18 +1,7 @@
 const { Router } = require("express");
 const router = Router();
-const moment = require("moment");
-const fs = require("fs");
-const path = require("path");
-const auth = require("../middleware/auth");
-const userCtrl = require("../controller/user");
-const holidays = require("./holidays");
-const { insertData, pool } = require("../db");
-const xlsx = require("xlsx");
 const fileUpload = require("express-fileupload");
-const bcryptjs = require("bcryptjs");
-const jsonwebtoken = require("jsonwebtoken");
 const dotenv = require("dotenv");
-const methods = require("../controller/authentication.controller");
 
 dotenv.config();
 router.use(fileUpload());
