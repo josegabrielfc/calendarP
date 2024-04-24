0ecd2edf7cb2d0a39b794cbd1d70d23caa175c14
jdprietom03 <jdprietom@eafit.edu.co>
2024-04-24 01:19:03 -0500

    Test deployment

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 
 const app = express();
 const api = require("./routes/index");
-const test = require("./api/test");
 
 //Settings
 app.set("port", process.env.PORT || 3001);
