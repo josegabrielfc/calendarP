45b6c5b2bbbb9f2e22d7b27811185975068fef04
nuvideveloper <jgfchona@gmail.com>
2024-05-05 11:15:57 -0500

    ajuste de cors en app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,10 @@ app.set("json spaces", 2);
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+    origin: true
+}));
 
 //routes
 app.use("/", api);
